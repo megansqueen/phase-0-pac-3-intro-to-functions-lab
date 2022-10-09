94cc2c6bdd02cbcfacfbc15a7241c8930f5a16f2
Megan <esler6724@gmail.com>
2022-10-09 12:50:13 -0700

    commit tree 6e553a15b6bf792867372e6832d28c0176a821c2

---
+++ index.js
@@ -0,0 +1,28 @@
+function shout(string) {
+    return string.toUpperCase()
+}
+
+function whisper(string) {
+    return string.toLowerCase()
+}
+
+const string = `Hello!`
+
+function logShout(string) {
+    console.log(string)
+    console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+    console.log(string)
+    console.log(string.toLowerCase())
+}
+
+function sayHiToHeadphonedRoommate(string) {
+    if (string === `Let's have dinner together!`)
+        return `I would love to!`
+    else if (string === string.toUpperCase())
+        return `YES INDEED!`
+    else if (string === string.toLowerCase())
+        return "I can't hear you!"
+}
